7c129bab93315fdae582c9e00c37752444c9bb1e
Hyun <38193576+sweetlulu486@users.noreply.github.com>
2018-12-03 19:33:42 +0900

    2

--- app/src/main/java/com/example/wsu/cs/drugsimpledic/DrugListAcitivity.java
+++ app/src/main/java/com/example/wsu/cs/drugsimpledic/DrugListAcitivity.java
@@ -1,4 +1,14 @@
 package com.example.wsu.cs.drugsimpledic;
 
-public class DrugListAcitivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DrugListAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
 }
--- app/src/main/java/com/example/wsu/cs/drugsimpledic/DrugVerifyActivity.java
+++ app/src/main/java/com/example/wsu/cs/drugsimpledic/DrugVerifyActivity.java
@@ -1,4 +1,14 @@
 package com.example.wsu.cs.drugsimpledic;
 
-public class DrugVerifyActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DrugVerifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
 }
